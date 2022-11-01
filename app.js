39659197d7f4d6520dced6ed0e127897c88f1bdb
TahoeBoelat <muhammadnuralfi98@gmail.com>
2022-11-01 09:44:01 +0700

    Adding complete state in index.html
    Adding style for complete state in style.css

---
+++ app.js
@@ -0,0 +1,22 @@
+// Card image section variable
+let cardName = document.getElementById("imgcardName")
+let cardNumber = document.getElementById("imgcardNumber")
+let cardexpMonth = document.getElementById("imgcardMonth")
+let cardexpYear = document.getElementById("imgcardYear")
+let cardCVV = document.getElementById("imgcardCVV")
+
+// Form section variable
+// Variable for visibility of the form
+let formSection = document.getElementsByClassName("form-section")
+let completeFormState = document.getElementsByClassName("complete-state-form")
+
+// Form input variable
+let inputCardName = document.getElementById("chinputName")
+let inputCardNumber = document.getElementById("cardinputNumber")
+let inputCardExpMonth = document.getElementById("cardinputMonth")
+let inputCardExpYear = document.getElementById("cardinputYear")
+let inputCardCVV = document.getElementById("cardinputcvv")
+
+// Function initialization
+
+console.log(formSection)
